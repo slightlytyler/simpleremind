04a99a994e89f35505e6c66d24bf34ea6dd79ae4
Tyler Martinez <slightlytyler@gmail.com>
2014-07-12 22:54:06 -0500

    simplified tab bar implementation

--- app/assets/javascripts/lib/components/tab-bar.js
+++ app/assets/javascripts/lib/components/tab-bar.js
@@ -1,16 +1,19 @@
-var tabBar = function tabBar(headerHeight, barObject) { 
-  var headerHeight = headerHeight;
-  var barObject = barObject;
+(function( $ ){
+  $.fn.tabBar = function(headerHeight) {
+    var headerHeight = headerHeight;
+    var barObject = this;
+    var pushObject = this.next('.tab-bar-push')
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > headerHeight) {
-      $(barObject).addClass('navbar-fixed-top');
-      $(barObject + '-push').addClass('show');
-    }
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > headerHeight) {
+        $(barObject).addClass('navbar-fixed-top');
+        $(pushObject).addClass('show');
+      }
 
-    if ($(window).scrollTop() < headerHeight) {
-      $(barObject).removeClass('navbar-fixed-top');
-      $(barObject + '-push').removeClass('show');
-    }
-  });
-};
+      if ($(window).scrollTop() < headerHeight) {
+        $(barObject).removeClass('navbar-fixed-top');
+        $(pushObject).removeClass('show');
+      }
+    });
+  };
+})(jQuery);
