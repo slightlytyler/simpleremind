ac627882c3b78e83c14be01b60d483d2b81f4407
Tyler Martinez <slightlytyler@gmail.com>
2014-07-16 14:26:26 -0500

    nav_link helper fixes, remindre list style updates, input short style updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
-  def nav_link(link_text, link_path)
+  def nav_link(link_path, &block)
     class_name = current_page?(link_path) ? 'active' : ''
 
     content_tag(:li, :class => class_name) do
-      link_to link_text, link_path
+       if block_given?
+        link_to link_path do
+          yield
+        end
+      end
     end
   end
 end
