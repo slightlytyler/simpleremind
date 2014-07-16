5416394ac4f29b3603c576fe92de04099b2b7275
Tyler Martinez <slightlytyler@gmail.com>
2014-07-15 21:32:53 -0500

    Fixed hash nav animation, implemented tabs on my account page, styled my account page

--- app/assets/javascripts/lib/utility/smooth-hash-nav.js
+++ app/assets/javascripts/lib/utility/smooth-hash-nav.js
@@ -1,7 +1,7 @@
 function smoothHashNav(fixedHeaderHeight) {
   var fixedHeaderHeight = fixedHeaderHeight;
   
-  $('a[href*=#]').click(function() {
+  $('a.hash-nav[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
     && location.hostname == this.hostname) {
       var $target = $(this.hash);
